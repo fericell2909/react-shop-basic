5a90af45c1c71b9ee9cc7574c27a5009f1715575
Marco Estrada Lopez <fericell2909@gmail.com>
2022-01-03 22:48:37 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <h1>
+            Hola Mundo Bieveneiedo al Curso practico de REACT
+        </h1>
+    )
+}
+
+export default App;
